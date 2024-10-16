f984f8f87a7d6789a223654a8f5ced8d9e234a24
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-16 14:12:40 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+fees=238000
+classes=48
+std_attendance=int(input("insert a number:"))
+std_fee_paid=int(input("insert a number:"))
+if fees>200000:
+    print("eligible")
+else:
+    print("non eligible")
+    
+if classes>40:
+    print("eligible")
+if classes<40:
+    print("non eligible")
